1dd2349d7938616537dfe4c48291f1e047175f88
KseniyaOsadcha <theksenic@gmail.com>
2021-03-14 00:36:41 +0200

    Добавьте файлы проекта.

---
+++ Lesson1_Create_Web_App/Config.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Lesson1_Create_Web_App
+{
+    public class Config
+    {
+        private readonly IConfiguration _conf;
+        public Config(IConfiguration conf)
+        {
+            _conf = conf;
+        }
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync(_conf.GetValue<string>("Logging:LogLevel:Microsoft.Hosting.Lifetime"));
+                });
+            });
+        }
+    }
+}
